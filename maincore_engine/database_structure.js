c67962b55971066c097cafc76436dacf18f82c16
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-17 00:25:53 +1000

    Create database_structure.js

---
+++ maincore_engine/database_structure.js
@@ -0,0 +1,22 @@
+const ApplicationStructure = {
+    database:{
+        table: [
+            {
+                name : 'user_test',
+                columns: [
+                    "name:r",
+                    "email:r",
+                    "password:r",
+                    "role:n"
+                ],
+                seeder: [
+                    ['Tommy Saputra Wijaya', 'tommy.wijaya54@yahoo.com','$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi'],
+                    ['Eko Saputra Wijaya', 'eko.saputra.wijaya@gmail.com','$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi'],
+                    ['Shinta Purnama Sari', 'shinta.purnama.sari@outlook.com','$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi'],
+                    ['Dewi Puspita Sari', 'dewi.puspita.sari@email.com','$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi']
+                ]
+            }
+        ]
+    }
+    
+}
