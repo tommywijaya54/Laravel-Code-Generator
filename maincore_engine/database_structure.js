64533610e05fb8b3a877961f162e6ce81642cbb3
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-17 03:32:47 +1000

    Adding more table for testing

--- maincore_engine/database_structure.js
+++ maincore_engine/database_structure.js
@@ -1,12 +1,21 @@
+/** 
+ *  Columns short hand 
+ *  entityname:[shorthand] 
+ *  example = email:n:f-email
+    :r = require (by default it has required)
+    :n = nullable
+    :f-[column_type] = format -> :f-string means the column will have string format
+*/
+
 const ApplicationStructure = {
     database:{
         table: [
             {
                 name : 'user_test',
-                columns: [
-                    "name:r",
-                    "email:r",
-                    "password:r",
+                column: [
+                    "name",
+                    "email",
+                    "password",
                     "role:n"
                 ],
                 seeder: [
@@ -14,6 +23,28 @@ const ApplicationStructure = {
                     ['Eko Saputra Wijaya', 'eko.saputra.wijaya@gmail.com','$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi'],
                     ['Shinta Purnama Sari', 'shinta.purnama.sari@outlook.com','$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi'],
                     ['Dewi Puspita Sari', 'dewi.puspita.sari@email.com','$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi']
+                ]
+            },
+            {
+                name : 'schedule',
+                column: [
+                    "day",
+                    "time_slot",
+                    "subject",
+                    "classroom",
+                    "duration",
+                    "date"
+                ]
+            },
+            {
+                name : 'registration',
+                column: [
+                    "student_id",
+                    "branch_id",
+                    "reference",
+                    "cashback",
+                    "status:n",
+                    "remarks:n"
                 ]
             }
         ]
