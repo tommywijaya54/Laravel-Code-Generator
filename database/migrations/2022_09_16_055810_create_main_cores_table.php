0ab9bae41ed07378a81f2d405e9bb6cae8b2b930
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-17 03:32:32 +1000

    Trying to use generated code

--- database/migrations/2022_09_16_055810_create_main_cores_table.php
+++ database/migrations/2022_09_16_055810_create_main_cores_table.php
@@ -3,6 +3,7 @@
 use App\Models\MainCore;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -23,6 +24,64 @@ return new class extends Migration
             $table->integer('created_by')->nullable();
             $table->timestamps();
         });
+
+        Schema::create('schedule', function (Blueprint $table) {
+            $table->id();
+            $table->date('day');
+            $table->string('time_slot');
+            $table->string('subject');
+            $table->string('classroom');
+            $table->string('duration');
+            $table->date('date');
+            $table->integer('created_by')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('registration', function (Blueprint $table) {
+            $table->id();
+            $table->integer('student_id');
+            $table->integer('branch_id');
+            $table->string('reference');
+            $table->string('cashback');
+            $table->string('status')->nullable();
+            $table->string('remarks')->nullable();
+            $table->integer('created_by')->nullable();
+            $table->timestamps();
+        });
+
+        function runSeederInBatch($seeder, $column, $tablename)
+        {
+            foreach ($seeder as $seed) {
+                $insert = [];
+                foreach ($column as $index => $col) {
+                    if (isset($seed[$index])) {
+                        $insert[$col] = $seed[$index];
+                    }
+                }
+                DB::table($tablename)->insert($insert);
+            }
+        };
+
+        $seeder = array(
+            array('Tommy Saputra Wijaya', 'tommy.wijaya54@yahoo.com', '$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi'),
+            array('Eko Saputra Wijaya', 'eko.saputra.wijaya@gmail.com', '$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi'),
+            array('Shinta Purnama Sari', 'shinta.purnama.sari@outlook.com', '$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi'),
+            array('Dewi Puspita Sari', 'dewi.puspita.sari@email.com', '$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi')
+        );
+        $column = array('name', 'email', 'password', 'role');
+        $tablename = "user_test";
+        runSeederInBatch($seeder, $column, $tablename);
+
+        $seeder = array(
+            array('Tommy Saputra Wijaya', 'tommy.wijaya54@yahoo.com', '$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi'),
+            array('Eko Saputra Wijaya', 'eko.saputra.wijaya@gmail.com', '$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi'),
+            array('Shinta Purnama Sari', 'shinta.purnama.sari@outlook.com', '$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi'),
+            array('Dewi Puspita Sari', 'dewi.puspita.sari@email.com', '$2y$10$5jVX3q8h6GnAqwN9KR9sVekmwYZQh0daV5.i65bzdXJMRYi/mtMZi')
+        );
+        $column = array('name', 'email', 'password', 'role');
+        $tablename = "user_test";
+
+        runSeederInBatch($seeder, $column, $tablename);
     }
 
     /**
